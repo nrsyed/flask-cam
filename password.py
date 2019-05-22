f817cb49029d36690fa8661fcf2ce3c408a7d253
Najam Syed <najam.r.syed@gmail.com>
2019-05-22 08:27:17 -0400

    Update authenticate function, add check_password

--- password.py
+++ password.py
@@ -22,17 +22,24 @@ def add_user(user, password, filepath):
         return True
     return False
 
-def authenticate_password(password, hash_):
-    return bcrypt.hashpw(password, hash_)
+def authenticate_password(password, hashed):
+    return bcrypt.hashpw(password.encode("utf-8"), hashed) == hashed
 
 def authenticate_user(user, password, filepath):
     if os.path.isfile(filepath):
         with open(filepath, "r") as file_:
             for line in file_:
-                user_, hash_ = line.split("\t")
-                if user == user_ and authenticate_password(password, hash_):
+                user_, hashed64 = line.strip().split("\t")
+                hashed = base64.b64decode(hashed64.encode("utf-8"))
+                if user == user_ and authenticate_password(password, hashed):
                     return True
     return False
+
+def delete_user(user, filepath):
+    if os.path.isfile(filepath):
+        with open(filepath, "r") as file_:
+            lines = file_.readlines()
+        print(lines)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -41,6 +48,7 @@ if __name__ == "__main__":
     action_group.add_argument("-a", "--add-user", action="store_true")
     action_group.add_argument("-d", "--delete-user", action="store_true")
     action_group.add_argument("-m", "--modify-user", action="store_true")
+    action_group.add_argument("-c", "--check-password", action="store_true")
 
     argparser.add_argument("-f", "--filepath", type=str, default="users")
     argparser.add_argument("-u", "--user", required=True, type=str)
@@ -54,3 +62,6 @@ if __name__ == "__main__":
     #    delete_user(args["user"], args["filepath"])
     #elif args["modify_user"]:
     #    modify_user(args["user"], args["password"], args["filepath"])
+    elif args["check_password"]:
+        val = authenticate_user(args["user"], args["password"], args["filepath"])
+        print(val)
