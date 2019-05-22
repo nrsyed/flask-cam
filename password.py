489fbc6fc5e5dbef21dc803ec7785ec06bb7523b
Najam Syed <najam.r.syed@gmail.com>
2019-05-21 22:32:11 -0400

    Add password authentication functions and argparser

--- password.py
+++ password.py
@@ -10,7 +10,7 @@ def add_user(user, password, filepath):
     if os.path.isfile(filepath):
         with open(filepath, "r") as file_:
             for line in file_:
-                user_, hashed = line.split("\t")
+                user_, hash_ = line.split("\t")
                 if user == user_:
                     user_in_file = True
 
@@ -22,11 +22,35 @@ def add_user(user, password, filepath):
         return True
     return False
 
-if __name__ == "__main__":
-    #argparser = argparse.ArgumentParser()
-    #argparser.add_argument("
-    user = sys.argv[1]
-    password = sys.argv[2]
-    filepath = sys.argv[3]
+def authenticate_password(password, hash_):
+    return bcrypt.hashpw(password, hash_)
 
-    add_user(user, password, filepath)
+def authenticate_user(user, password, filepath):
+    if os.path.isfile(filepath):
+        with open(filepath, "r") as file_:
+            for line in file_:
+                user_, hash_ = line.split("\t")
+                if user == user_ and authenticate_password(password, hash_):
+                    return True
+    return False
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+
+    action_group = argparser.add_mutually_exclusive_group()
+    action_group.add_argument("-a", "--add-user", action="store_true")
+    action_group.add_argument("-d", "--delete-user", action="store_true")
+    action_group.add_argument("-m", "--modify-user", action="store_true")
+
+    argparser.add_argument("-f", "--filepath", type=str, default="users")
+    argparser.add_argument("-u", "--user", required=True, type=str)
+    argparser.add_argument("-p", "--password", type=str)
+
+    args = vars(argparser.parse_args())
+
+    if args["add_user"]:
+        add_user(args["user"], args["password"], args["filepath"])
+    #elif args["delete_user"]:
+    #    delete_user(args["user"], args["filepath"])
+    #elif args["modify_user"]:
+    #    modify_user(args["user"], args["password"], args["filepath"])
