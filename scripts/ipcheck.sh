b97e7538b2c6173c3a3adf1e609d22fca35d4206
Najam Syed <najam.r.syed@gmail.com>
2019-06-09 10:29:28 -0400

    Add IP check script and cron job

---
+++ scripts/ipcheck.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Where this script lives, i.e., the /scripts directory.
+SCRIPTS_DIR=$(readlink -f $(dirname "$0"))
+
+# Main application directory one level up from this one.
+APP_DIR=$(readlink -f "$SCRIPTS_DIR/..")
+
+# Application /tmp directory in flask-cam/tmp.
+APP_TMP_DIR="$APP_DIR/tmp"
+
+# Path to file containing previous IP address in flask-cam/tmp.
+FILEPATH="$APP_TMP_DIR/ipaddress"
+
+# Get current IP address.
+IP_ADDR=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+# Create flask-cam/tmp if doesn't exist.
+[[ -d "$APP_TMP_DIR" ]] || mkdir -p "$APP_TMP_DIR"
+
+# Redirect current IP address to flask-cam/tmp/ipaddress if file doesn't exist
+# or if file exists and current IP address different from previous.
+if [ ! -f "$FILEPATH" ] || [ "$IP_ADDR" != "$(cat "$FILEPATH")" ]; then
+  echo "IP changed"
+  echo $IP_ADDR > "$FILEPATH"
+fi
---
+++ scripts/make_ipcheck_cronjob.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Where this script lives, i.e., the /scripts directory.
+SCRIPTS_DIR=$(readlink -f $(dirname "$0"))
+
+# Add a file to /etc/cron.d for a job that runs flask-cam/scripts/ipcheck.sh
+# every 15 minutes as the current user and logs any output to syslog under the
+# name 'IPCHECK'.
+echo "*/15 * * * * $USER $SCRIPTS_DIR/ipcheck.sh 2>&1 | /usr/bin/logger -t IPCHECK" | sudo tee /etc/cron.d/ipcheck > /dev/null
