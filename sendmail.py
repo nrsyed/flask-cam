dab6547f1a8c1a119a9b158ade937cb5e7ddf327
Najam Syed <najam.r.syed@gmail.com>
2019-06-09 11:37:33 -0400

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,32 +1,46 @@
+import argparse
 import email.utils
-import json
-import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import json
+import smtplib
 
 def import_secrets(filename):
     with open(filename, "r") as file_:
         secrets = json.load(file_)
     return secrets
 
-def send_mail(secrets, subject, body):
+def send_mail(
+    sender, sender_name, recipient, smtp_username, smtp_password, host, port,
+    subject, body
+):
     msg = MIMEMultipart("alternative")
     msg["Subject"] = subject
     msg["From"] = email.utils.formataddr(
-        (secrets["sender_name"], secrets["sender"])
+        (sender_name, sender)
     )
-    msg["To"] = secrets["recipient"]
+    msg["To"] = recipient
     msg.attach(MIMEText(body, "plain"))
 
     try:
-        server = smtplib.SMTP(secrets["host"], secrets["port"])
+        server = smtplib.SMTP(host, port)
         server.ehlo()
         server.starttls()
         server.ehlo()
-        server.login(secrets["smtp_username"], secrets["smtp_password"])
-        server.sendmail(secrets["sender"], secrets["recipient"], msg.as_string())
+        server.login(smtp_username, smtp_password)
+        server.sendmail(sender, recipient, msg.as_string())
         server.close()
     except Exception as e:
         print(e)
     else:
         print("success")
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-b", "--body", type=str, default="")
+    argparser.add_argument("-s", "--subject", type=str, default="")
+
+    args = argparser.parse_args()
+    secrets = import_secrets("secrets")
+
+    send_mail(body=args.body, subject=args.subject, **secrets)
