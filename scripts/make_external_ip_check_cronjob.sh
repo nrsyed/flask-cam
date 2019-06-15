dbca319faf43ad282cc6530da62cada793710b78
Najam Syed <najam.r.syed@gmail.com>
2019-06-14 22:48:55 -0400

    Rename external IP check scripts for clarity

--- scripts/make_external_ip_check_cronjob.sh
+++ scripts/make_external_ip_check_cronjob.sh
@@ -6,4 +6,4 @@ SCRIPTS_DIR=$(readlink -f $(dirname "$0"))
 # Add a file to /etc/cron.d for a job that runs flask-cam/scripts/ipcheck.sh
 # every 15 minutes as the current user and logs any output to syslog under the
 # name 'IPCHECK'.
-echo "*/15 * * * * $USER $SCRIPTS_DIR/ipcheck.sh 2>&1 | /usr/bin/logger -t IPCHECK" | sudo tee /etc/cron.d/ipcheck > /dev/null
+echo "*/15 * * * * $USER $SCRIPTS_DIR/external_ip_check.sh 2>&1 | /usr/bin/logger -t IPCHECK" | sudo tee /etc/cron.d/external_ip_check > /dev/null
