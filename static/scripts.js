4902d83c95b036394860e398d55aadad1fa22218
Najam Syed <najam.r.syed@gmail.com>
2019-06-16 08:49:48 -0400

    Fix scripts.js indentation

--- static/scripts.js
+++ static/scripts.js
@@ -1,27 +1,27 @@
 function formSubmit(event) {
-	event.preventDefault();
-	let url = "/submit";
-	let request = new XMLHttpRequest();
-	let formData = new FormData();
+  event.preventDefault();
+  let url = "/submit";
+  let request = new XMLHttpRequest();
+  let formData = new FormData();
 
-	for (input of event.target.querySelectorAll("input")) {
-		formData.append(input.id, input.value);
-	}
+  for (input of event.target.querySelectorAll("input")) {
+  	formData.append(input.id, input.value);
+  }
 
-	sendRequest(formData);
+  sendRequest(formData);
 }
 
 function sendRequest(formData) {
-	let url = "/submit";
-	let request = new XMLHttpRequest();
-	request.open("POST", url, true);
-	request.send(formData);
-	request.onload = function() {
-		console.log(request.responseText);
-	};
-	request.onerror = function() {
-		console.log("POST error");
-	};
+  let url = "/submit";
+  let request = new XMLHttpRequest();
+  request.open("POST", url, true);
+  request.send(formData);
+  request.onload = function() {
+  	console.log(request.responseText);
+  };
+  request.onerror = function() {
+  	console.log("POST error");
+  };
 }
 
 function getControlValues() {
@@ -33,15 +33,14 @@ function getControlValues() {
     let response = JSON.parse(request.responseText);
     for (let control in response) {
       let indicator = document.getElementById(control);
-      console.log(control);
-      console.log(indicator);
+      indicator.textContent = response[control];
     }
   };
 }
 
 function init() {
-	form = document.getElementById("controls");
-	form.addEventListener("submit", formSubmit);
+  form = document.getElementById("controls");
+  form.addEventListener("submit", formSubmit);
 
   getControlValues();
 }
