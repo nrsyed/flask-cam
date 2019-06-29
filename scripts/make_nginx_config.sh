b389695b66f184f361303b8616233de65adc0533
Najam Syed <najam.r.syed@gmail.com>
2019-06-29 00:08:42 -0400

    Add SSL configuration setup to make_nginx_config.sh

--- scripts/make_nginx_config.sh
+++ scripts/make_nginx_config.sh
@@ -33,6 +33,11 @@ if $UNINSTALL; then
   sudo ufw delete allow $PORT
   [[ ! -f /etc/nginx/sites-enabled/flaskcam ]] || sudo rm /etc/nginx/sites-enabled/flaskcam
   [[ ! -f /etc/nginx/sites-available/flaskcam ]] || sudo rm /etc/nginx/sites-available/flaskcam
+  [[ ! -f /etc/nginx/snippets/self-signed.conf ]] || sudo rm /etc/nginx/snippets/self-signed.conf
+  [[ ! -f /etc/nginx/snippets/ssl-params.conf ]] || sudo rm /etc/nginx/snippets/ssl-params.conf
+  [[ ! -f /etc/ssl/private/self-signed.key ]] || sudo rm /etc/ssl/private/self-signed.key
+  [[ ! -f /etc/ssl/certs/self-signed.crt ]] || sudo rm /etc/ssl/certs/self-signed.crt
+  [[ ! -f /etc/ssl/certs/dhparam.pem ]] || sudo rm /etc/ssl/certs/dhparam.pem
   exit 0
 fi
 
@@ -40,6 +45,13 @@ if [[ -z "$PORT" ]]; then
   echo "Error: port required (use --port)"
   exit 1
 fi
+
+# Generate a self-signed SSL key and certificate, and Diffie-Helman group.
+EXTERNAL_IP_ADDR=$(dig +short myip.opendns.com @resolver1.opendns.com)
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -subj "/C=US/CN=$EXTERNAL_IP_ADDR" \
+  -keyout /etc/ssl/private/self-signed.key -out /etc/ssl/certs/self-signed.crt
+sudo openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 2048
 
 # Addresses in the private IP space begin with one of three blocks:
 # 10.X.X.X
@@ -51,9 +63,36 @@ fi
 REGEX='inet [a-zA-Z\:]*(192\.168|10\.[0-9]+|172\.1[6789]|172\.2[0-9]|172\.3[01])\.[0-9]+\.[0-9]+'
 LOCAL_IP_ADDR=$(ifconfig | egrep -o "$REGEX" | egrep -o '[0-9]+.*[0-9]+')
 
+# Create nginx config snippet pointing to SSL key and certificate.
+echo "ssl_certificate /etc/ssl/certs/self-signed.crt;
+ssl_certificate_key /etc/ssl/private/self-signed.key;" \
+  | sudo tee /etc/nginx/snippets/self-signed.conf > /dev/null
+
+# Generate ssl-params.conf based on settings from: https://cipherli.st,
+# https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html, and
+# https://digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-on-debian-8
+echo "ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+ssl_prefer_server_ciphers on;
+ssl_ciphers \"EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH\";
+ssl_ecdh_curve secp384r1;
+ssl_session_cache shared:SSL:10m;
+ssl_session_tickets off;
+ssl_stapling on;
+ssl_stapling_verify on;
+resolver 8.8.8.8 8.8.4.4 valid=300s;
+resolver_timeout 5s;
+add_header Strict-Transport-Security \"max-age=63072000; includeSubdomains\";
+add_header X-Frame-Options DENY;
+add_header X-Content-Type-Options nosniff;
+ssl_dhparam /etc/ssl/certs/dhparam.pem;" \
+  | sudo tee /etc/nginx/snippets/ssl-params.conf > /dev/null
+
+# Create nginx config file, allowing only HTTPS access.
 echo "server {
-    listen $PORT;
-    server_name $LOCAL_IP_ADDR
+    listen $PORT ssl;
+    server_name $LOCAL_IP_ADDR;
+    include snippets/self-signed.conf;
+    include snippets/ssl-params.conf;
 
     location / {
         include proxy_params;
