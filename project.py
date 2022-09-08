5db562138aca71e170acb9980270b9ec30c98a95
Bread White <breadrock1@gmail.com>
2022-09-08 13:18:12 +0300

    Created project.py file

---
+++ project.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_DIR_PATH = Path('.').absolute()
+
+RESOURCES_DIR_PATH = ROOT_DIR_PATH / 'resources'
+STATIC_DIR_PATH = ROOT_DIR_PATH / 'static'
+TEMPLATES_DIR_PATH = ROOT_DIR_PATH / 'templates'
