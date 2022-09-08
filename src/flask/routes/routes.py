18d7f07a81323e2e877bf5795b88dedd84d65054
Bread White <breadrock1@gmail.com>
2022-09-08 13:19:21 +0300

    Removed auth method + Added load_statistic_data() method

--- src/flask/routes/routes.py
+++ src/flask/routes/routes.py
@@ -1,10 +1,6 @@
-from syslog import syslog
-from functools import wraps
+from distutils.util import strtobool
 from time import sleep as time_sleep
 
-from ipaddress import ip_address, ip_network
-
-from werkzeug import exceptions
 from flask import (
     Flask,
     Response,
@@ -15,11 +11,15 @@ from flask import (
 )
 
 from src.flask.camera.camera import Camera
-from src.auth.auth_manager import authenticate_user
+from src.flask.statistic.anxiety_statistic import AnxietyStatistic
+
+from project import TEMPLATES_DIR_PATH, STATIC_DIR_PATH
 
 
-flask_application = Flask(__name__)
+flask_application = Flask(__name__, template_folder=TEMPLATES_DIR_PATH.__str__(), static_folder=STATIC_DIR_PATH)
+
 camera = Camera()
+anxiety_statistic = AnxietyStatistic(saved_data=[])
 
 
 @flask_application.before_first_request
@@ -27,62 +27,10 @@ def initialize():
     current_app.delay = 0.1
 
 
-def unauthorized():
-    return Response("Credential error", 401, {"WWW-Authenticate": "Basic realm='Login Required'"})
-
-
-def requires_auth(func):
-    """
-    Decorator for routes that require authentication.
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        # Running a script from Python with subprocess via gunicorn seems to
-        # present many issues, including lack of PATH and inability to resolve
-        # paths even when `shell=True` is passed. Instead, write IP/mask to file
-        # on network up and periodically via network and cron jobs; we check
-        # the contents of said file here to obtain the local IP and subnet mask.
-        # `local_network` file contains IP/subnet in format
-        # `XXX.XXX.XXX.XXX/YYY.YYY.YYY.YYY`.
-
-        subnet = None
-        # filepath = path.join(path[0], "tmp/local_network")
-        filepath = "/tmp/local_network"
-        try:
-            with open(filepath, "r") as file_:
-                local_ip_and_mask = file_.readline().strip()
-            subnet = ip_network(local_ip_and_mask, strict=False)
-        except Exception as e:
-            syslog(str(e))
-
-        # Get IP address of requester using nginx header. Do not require
-        # authentication for requests originating on the local network.
-        remote_addr = request.environ.get("HTTP_X_FORWARDED_FOR")
-        syslog("request from {}".format(remote_addr))
-        if remote_addr is None or subnet is None or ip_address(remote_addr) not in subnet:
-            auth = request.authorization
-            if not auth or not authenticate_user(auth.username, auth.password, "users"):
-                return unauthorized()
-        return func(*args, **kwargs)
-    return wrapper 
-
-
 @flask_application.route("/")
-@requires_auth
+# @requires_auth
 def index():
     return render_template("index.html")
-
-
-@flask_application.route("/kiosk")
-@requires_auth
-def kiosk():
-    return render_template("kiosk.html")
-
-
-@flask_application.errorhandler(exceptions.BadRequest)
-def handle_bad_request(e):
-    return f'bad request: {e}', 400
 
 
 @flask_application.route("/get", methods=["GET"])
@@ -156,3 +104,16 @@ def gen():
         with flask_application.app_context():
             # Add nominal delay between frames to prevent performance issues.
             time_sleep(current_app.delay)
+
+
+@flask_application.route('/statistic', methods=['POST', 'GET'])
+def load_statistic_data():
+    if request.method == 'POST':
+        post_data = [bool(strtobool(elem)) for elem in request.get_json(force=True)]
+        anxiety_statistic.add_anxiety_statistic(post_data)
+        return jsonify({'message': 'success'})
+
+    if request.method == 'GET':
+        return jsonify(anxiety_statistic.get_anxiety_statistic)
+
+    return jsonify({'message': 'error'})
